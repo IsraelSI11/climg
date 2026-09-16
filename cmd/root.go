a30b0c3af5dc3df1f57a3fe88c756cd2de48f228
IsraelSI11 <elplayerhd@hotmail.es>
2026-09-16 23:25:53 +0200

    Scaffold climg CLI with Cobra and add upload command
    Bootstraps the Go module and Cobra project structure, plus a first
    `upload` subcommand that validates a local file path exists before
    any AWS integration is wired in.

---
+++ cmd/root.go
@@ -0,0 +1,45 @@
+/*
+Copyright © 2026 Israel
+*/
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "climg",
+	Short: "Upload and Manage optimized images in S3",
+	Long: `Upload and Manage optimized images in S3. 
+	
+	This CLI is meant to be used for humans and agents, the main purpose of this tool
+	is to enhance the security and performance of the management of webapps assets.
+	`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	// Run: func(cmd *cobra.Command, args []string) { },
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.climg.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
---
+++ main.go
@@ -0,0 +1,11 @@
+/*
+Copyright © 2026 Israel
+
+*/
+package main
+
+import "github.com/IsraelSI11/climg/cmd"
+
+func main() {
+	cmd.Execute()
+}
